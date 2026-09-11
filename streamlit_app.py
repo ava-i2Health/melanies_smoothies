d3de9edbb6950eb50ab31264e3bde5e37be11cba
ava-i2Health <ava.ambuske@i2health.com>
2026-09-11 16:14:42 -0700

    Fix typo in dataframe variable name

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 # Convert the snowpack dataframe to a pandas dataframe so we can use the LOC function
-pd_df=my_datafram.to_pandas()
+pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
